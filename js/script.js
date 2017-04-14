378f9c7e4e28cf170e6ea6d170606fc807aa19ba
Algirdas <Algirdas.j@gmail.com>
2017-04-14 10:54:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,225 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    })
+
+})(jQuery); // End of use strict
+
+// v3.1.0
+//Docs at http://simpleweatherjs.com
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'Vilnius, Lithuania',
+    woeid: '',
+    unit: 'c',
+    success: function(weather) {
+      html = '<h2><div class="icon icon-'+weather.code+'"></div> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      //html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      //html += '<li class="currently">'+weather.currently+'</li>';
+      //html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
+
+
+// Clock
+window.onload = function () {
+        setInterval(showClock, 1000);
+
+        function showClock() {
+
+            // DEFINE CANVAS AND ITS CONTEXT.
+            var canvas = document.getElementById('clock');
+            var ctx = canvas.getContext('2d');
+
+            var date = new Date;
+            var angle;
+            var secHandLength = 150;
+
+            // CLEAR EVERYTHING ON THE CANVAS. RE-DRAW NEW ELEMENTS EVERY SECOND.
+            ctx.clearRect(0, 0, canvas.width, canvas.height);        
+
+            //OUTER_DIAL1();
+            OUTER_DIAL2();
+            CENTER_DIAL();
+            MARK_THE_HOURS();
+            //MARK_THE_SECONDS();
+
+            SHOW_SECONDS();
+            SHOW_MINUTES();
+            SHOW_HOURS();
+			SHOW_CITY();
+			SHOW_FULL_CITY_AND_TIME();
+
+
+            function OUTER_DIAL1() {
+                ctx.beginPath();
+                ctx.arc(canvas.width / 2, canvas.height / 2, secHandLength + 10, 0, Math.PI * 2);
+                ctx.strokeStyle = '#fff';
+                ctx.stroke();
+            }
+            function OUTER_DIAL2() {
+                ctx.beginPath();
+                ctx.arc(canvas.width / 2, canvas.height / 2, secHandLength + 7, 0, Math.PI * 2);
+                ctx.strokeStyle = '#fff';
+				ctx.lineWidth = 8; 
+                ctx.stroke();
+            }
+            function CENTER_DIAL() {
+                ctx.beginPath();
+                ctx.arc(canvas.width / 2, canvas.height / 2, 2, 0, Math.PI * 2);
+                ctx.lineWidth = 4;
+                ctx.fillStyle = '#fff';
+                ctx.strokeStyle = '#fff';
+                ctx.stroke();
+            }
+			
+			function SHOW_CITY() {
+				ctx.fillStyle = '#fff';
+				ctx.font = "bold 100px Arial";
+				ctx.textAlign="center";
+				ctx.textBaseline="middle"; 
+				ctx.fillText("VLN",canvas.width / 2, canvas.height / 2);
+				ctx.stroke();
+			}
+			
+			function SHOW_FULL_CITY_AND_TIME(){
+				ctx.fillStyle = '#fff';
+				ctx.font = "14px Arial";
+				var hours = date.getHours();
+				var minutes = date.getMinutes();
+				ctx.fillText("LITHUANIA, VILNIUS | " + hours + ":" + minutes, canvas.width / 2, canvas.height / 1.6);
+				ctx.stroke();
+			}
+			
+
+            function MARK_THE_HOURS() {
+
+                for (var i = 0; i < 12; i++) {
+                    angle = (i - 3) * (Math.PI * 2) / 12;       // THE ANGLE TO MARK.
+                    ctx.lineWidth = 1;            // HAND WIDTH.
+                    ctx.beginPath();
+
+                    var x1 = (canvas.width / 2) + Math.cos(angle) * (secHandLength);
+                    var y1 = (canvas.height / 2) + Math.sin(angle) * (secHandLength);
+                    var x2 = (canvas.width / 2) + Math.cos(angle) * (secHandLength - (secHandLength / 7));
+                    var y2 = (canvas.height / 2) + Math.sin(angle) * (secHandLength - (secHandLength / 7));
+
+                    ctx.moveTo(x1, y1);
+                    ctx.lineTo(x2, y2);
+
+                    ctx.strokeStyle = '#fff';
+                    ctx.stroke();
+                }
+            }
+
+            function MARK_THE_SECONDS() {
+
+                for (var i = 0; i < 60; i++) {
+                    angle = (i - 3) * (Math.PI * 2) / 60;       // THE ANGLE TO MARK.
+                    ctx.lineWidth = 1;            // HAND WIDTH.
+                    ctx.beginPath();
+
+                    var x1 = (canvas.width / 2) + Math.cos(angle) * (secHandLength);
+                    var y1 = (canvas.height / 2) + Math.sin(angle) * (secHandLength);
+                    var x2 = (canvas.width / 2) + Math.cos(angle) * (secHandLength - (secHandLength / 30));
+                    var y2 = (canvas.height / 2) + Math.sin(angle) * (secHandLength - (secHandLength / 30));
+
+                    ctx.moveTo(x1, y1);
+                    ctx.lineTo(x2, y2);
+
+                    ctx.strokeStyle = '#fff';
+                    ctx.stroke();
+                }
+            }
+
+            function SHOW_SECONDS() {
+
+                var sec = date.getSeconds();
+                angle = ((Math.PI * 2) * (sec / 60)) - ((Math.PI * 2) / 4);
+                ctx.lineWidth = 0.5;              // HAND WIDTH.
+
+                ctx.beginPath();
+                // START FROM CENTER OF THE CLOCK.
+                ctx.moveTo(canvas.width / 2, canvas.height / 2);   
+                // DRAW THE LENGTH.
+                ctx.lineTo((canvas.width / 2 + Math.cos(angle) * secHandLength),
+                    canvas.height / 2 + Math.sin(angle) * secHandLength);
+
+                // DRAW THE TAIL OF THE SECONDS HAND.
+                ctx.moveTo(canvas.width / 2, canvas.height / 2);    // START FROM CENTER.
+                // DRAW THE LENGTH.
+                ctx.lineTo((canvas.width / 2 - Math.cos(angle) * 20),
+                    canvas.height / 2 - Math.sin(angle) * 20);
+
+                ctx.strokeStyle = '#fff';        // COLOR OF THE HAND.
+                ctx.stroke();
+            }
+
+            function SHOW_MINUTES() {
+
+                var min = date.getMinutes();
+                angle = ((Math.PI * 2) * (min / 60)) - ((Math.PI * 2) / 4);
+                ctx.lineWidth = 3;              // HAND WIDTH.
+
+                ctx.beginPath();
+                ctx.moveTo(canvas.width / 2, canvas.height / 2);  // START FROM CENTER.
+                // DRAW THE LENGTH.
+                ctx.lineTo((canvas.width / 2 + Math.cos(angle) * secHandLength / 1.1),      
+                    canvas.height / 2 + Math.sin(angle) * secHandLength / 1.1);
+
+                ctx.strokeStyle = '#fff';  // COLOR OF THE HAND.
+                ctx.stroke();
+            }
+
+            function SHOW_HOURS() {
+
+                var hour = date.getHours();
+                var min = date.getMinutes();
+				var hourOverhang = 20;
+				
+                angle = ((Math.PI * 2) * ((hour * 5 + (min / 60) * 5) / 60)) - ((Math.PI * 2) / 4);
+                ctx.lineWidth = 6;              // HAND WIDTH.
+
+                ctx.beginPath();
+                ctx.moveTo(canvas.width / 2, canvas.height / 2);     // START FROM CENTER.
+                // DRAW THE LENGTH.
+                ctx.lineTo((canvas.width / 2 + Math.cos(angle) * secHandLength / 1.5),      
+                    canvas.height / 2 + Math.sin(angle) * secHandLength / 1.5);
+
+                ctx.strokeStyle = '#fff';   // COLOR OF THE HAND.
+                ctx.stroke();
+            }
+        }
+    }
+
